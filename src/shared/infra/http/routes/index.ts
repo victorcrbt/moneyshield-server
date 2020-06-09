f062ae60d69fde6c6d7689a594153aaa446e0c87
Victor <madivcb@gmail.com>
2020-06-08 22:34:54 -0300

    chore: add route to create a new user

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -4,8 +4,6 @@ import usersRouter from '@modules/users/infra/http/routes/users.routes';
 
 const routes = Router();
 
-routes.use(usersRouter);
-
-routes.get('/', (req, res) => res.send('Hello, Worlds!'));
+routes.use('/users', usersRouter);
 
 export default routes;
