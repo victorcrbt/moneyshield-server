9bc9d8a012d16344bae5bb430c97919f00976510
Victor <madivcb@gmail.com>
2020-06-08 22:56:35 -0300

    chore: add method to compare hashes

--- src/modules/users/providers/HashProvider/fakes/FakeHashProvider.ts
+++ src/modules/users/providers/HashProvider/fakes/FakeHashProvider.ts
@@ -1,9 +1,17 @@
 import IHashProvider from '../models/IHashProvider';
 
 import IGenerateHashDTO from '../dtos/IGenerateHashDTO';
+import ICompareHashDTO from '../dtos/ICompareHashDTO';
 
 export default class FakeHashProvider implements IHashProvider {
   public async generateHash({ payload }: IGenerateHashDTO): Promise<string> {
     return payload;
+  }
+
+  public async compareHash({
+    payload,
+    hash,
+  }: ICompareHashDTO): Promise<boolean> {
+    return payload === hash;
   }
 }
--- src/modules/users/providers/HashProvider/implementations/BCryptHashProvider.ts
+++ src/modules/users/providers/HashProvider/implementations/BCryptHashProvider.ts
@@ -3,11 +3,19 @@ import bcrypt from 'bcryptjs';
 import IHashProvider from '../models/IHashProvider';
 
 import IGenerateHashDTO from '../dtos/IGenerateHashDTO';
+import ICompareHashDTO from '../dtos/ICompareHashDTO';
 
 export default class BCryptHashProvider implements IHashProvider {
   public async generateHash({ payload }: IGenerateHashDTO): Promise<string> {
     const hashedValue = await bcrypt.hash(payload, 10);
 
     return hashedValue;
+  }
+
+  public async compareHash({
+    payload,
+    hash,
+  }: ICompareHashDTO): Promise<boolean> {
+    return bcrypt.compare(payload, hash);
   }
 }
