40036f67e70fe4261b7962af2ff82bdb57dc137c
Victor <madivcb@gmail.com>
2020-06-20 16:50:25 -0300

    feat: add route to toggle the status of a financial movement

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -8,6 +8,7 @@ import passwordRouter from '@modules/users/infra/http/routes/password.routes';
 import cashiersRouter from '@modules/cashiers/infra/http/routes/cashiers.routes';
 
 import financialMovementsRouter from '@modules/financial_movements/infra/http/routes/financialMovements.routes';
+import paymentStatusRouter from '@modules/financial_movements/infra/http/routes/paymentStatus.routes';
 
 const routes = Router();
 
@@ -19,5 +20,6 @@ routes.use('/profiles', profilesRouter);
 routes.use('/cashiers', cashiersRouter);
 
 routes.use('/financial_movements', financialMovementsRouter);
+routes.use('/payment_status', paymentStatusRouter);
 
 export default routes;
